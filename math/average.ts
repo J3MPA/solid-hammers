b23c0991d7e356ef5f577d6ddb532b293dfcb0ca
Jens Karlsson <jens_karlsson95@hotmail.com>
2021-03-19 12:38:32 +0100

    adds average method

--- math/average.ts
+++ math/average.ts
@@ -1,5 +1,17 @@
+import ifThis from '../functions/assert/ifThis'
+import isFalse from '../functions/assert/isFalse'
+import { isRealNumber } from './utils'
+
+const throwTypeError = (val: any) => {
+  throw new TypeError(`"${val}" is not a real number`)
+}
+
+const isNotARealNumber = (num: any) => isFalse(isRealNumber(num))
+
 const average = (nthNumbers: number[]) =>
   nthNumbers.reduce((acc, number, i, arr) => {
+    ifThis(isNotARealNumber, throwTypeError, number)
+
     const nth = arr.length
     const isLast = i === nth - 1
     if (isLast) return (acc + number) / nth
--- math/tests/average.spec.ts
+++ math/tests/average.spec.ts
@@ -6,7 +6,44 @@ describe('average', () => {
       expect(average([10, 10, 10])).toBe(10)
       expect(average([20, 5, 5])).toBe(10)
       expect(average([5, 15, 10])).toBe(10)
-      expect(average([65, 78, 63, 352])).toBe(10)
+      expect(average([65, 78, 63, 352])).toBe(139.5)
+    })
+  })
+
+  describe('Sad path', () => {
+    it('should trow type error if one of given numbers is not a real number', () => {
+      expect(() => average([10, '10', 10] as any)).toThrowError(TypeError)
+      expect(() =>
+        average([10, '10', 10] as any)
+      ).toThrowErrorMatchingSnapshot()
+      expect(() => average([20, 5, null] as any)).toThrowError(TypeError)
+      expect(() => average([20, 5, null] as any)).toThrowErrorMatchingSnapshot()
+      expect(() => average([5, 15, undefined] as any)).toThrowError(TypeError)
+      expect(() =>
+        average([5, 15, undefined] as any)
+      ).toThrowErrorMatchingSnapshot()
+      expect(() => average([65, 78, 63, NaN] as any)).toThrowError(TypeError)
+      expect(() =>
+        average([65, 78, 63, NaN] as any)
+      ).toThrowErrorMatchingSnapshot()
+      expect(() => average([65, 78, 63, Infinity] as any)).toThrowError(
+        TypeError
+      )
+      expect(() =>
+        average([65, 78, 63, Infinity] as any)
+      ).toThrowErrorMatchingSnapshot()
+      expect(() => average([65, 78, 63, []] as any)).toThrowError(TypeError)
+      expect(() =>
+        average([65, 78, 63, []] as any)
+      ).toThrowErrorMatchingSnapshot()
+      expect(() => average([65, 78, 63, {}] as any)).toThrowError(TypeError)
+      expect(() =>
+        average([65, 78, 63, {}] as any)
+      ).toThrowErrorMatchingSnapshot()
+      expect(() => average([65, 78, 63, ''] as any)).toThrowError(TypeError)
+      expect(() =>
+        average([65, 78, 63, ''] as any)
+      ).toThrowErrorMatchingSnapshot()
     })
   })
 })
