330607378a654692bca4148bc7759e516dbad1a7
Jens Karlsson <jens_karlsson95@hotmail.com>
2021-07-12 22:42:19 +0200

    removes math utils

--- math/utils/isRealNumber.ts
+++
@@ -1,2 +0,0 @@
-const isRealNumber = (n: number) => Number.isFinite(n)
-export default isRealNumber
--- math/utils/isRealPoint.ts
+++
@@ -1,5 +0,0 @@
-import isRealNumber from './isRealNumber'
-
-const isRealPoint = (x: number, y: number) => isRealNumber(x) && isRealNumber(y)
-
-export default isRealPoint
